2204aba833144943b3d49ff670cb91feb33203d0
Mariionde <school@fonmatte.com>
2019-04-21 12:40:07 +0300

    lesson_1

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -20,5 +20,6 @@ import mainapp.views as mainapp
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', mainapp.main),
-
+    path('products/', mainapp.products),
+    path('contacts/', mainapp.contacts),
 ]
